c6f28ecb861003f135ea8ec5eba2be69df07ed73
Kex <krzysieksobol9@gmail.com>
2024-07-04 17:13:30 +0200

    innit

---
+++ src/main/java/com/krzysztofsobol/cvwebsite/CvwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.krzysztofsobol.cvwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krzysztofsobol/cvwebsite/CvwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krzysztofsobol.cvwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
