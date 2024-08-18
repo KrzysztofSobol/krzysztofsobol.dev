d71fec0c7ac2fec84d0fa3c7fe19ef4d34b50045
Kex <125202700+KrzysztofSobol@users.noreply.github.com>
2024-08-18 23:51:40 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             setHeaderHeight(headerRef.current.offsetHeight);
         }
     }, []);
-
+ 
     return (
         <div className="map-container">
             <Map />
