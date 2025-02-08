e46a9ca693954f784e8ba200d096d5d165baf4c1
Kex <krzysieksobol9@gmail.com>
2025-02-08 12:42:33 +0100

    changed scroll buttonScroll.tsx

--- frontend/src/Heropage/buttonScroll.tsx
+++ frontend/src/Heropage/buttonScroll.tsx
@@ -18,22 +18,22 @@ function ScrollButton() {
                  viewBox="180.59 105.544 16.095 16.622" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M 195.635 106.566 L 189.342 112.858 C 188.951 113.249 188.318 113.249 187.927 112.858 L 181.635 106.566"
-                    strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
                     style={{
                         paintOrder: 'fill',
                         fillRule: 'nonzero',
                         fill: 'rgba(255, 255, 255, 0)',
-                        stroke: 'rgb(15 44 55)'
+                        stroke: 'rgb(255 255 255)'
                     }}
                 />
                 <path
                     d="M 195.635 114.566 L 189.342 120.858 C 188.951 121.249 188.318 121.249 187.927 120.858 L 181.635 114.566"
-                    strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
                     style={{
                         paintOrder: 'fill',
                         fillRule: 'nonzero',
                         fill: 'rgba(252, 0, 0, 0)',
-                        stroke: 'rgb(15 44 55)'
+                        stroke: 'rgb(255 255 255)'
                     }}
                 />
             </svg>
