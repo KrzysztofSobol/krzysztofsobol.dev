75646d29ec8e98caf9c8e14ded14affc91052733
Kex <krzysieksobol9@gmail.com>
2025-02-19 21:00:59 +0100

    contact.tsx added

--- frontend/src/Contact/contact.tsx
+++ frontend/src/Contact/contact.tsx
@@ -1,71 +1,55 @@
-import "@/Contact/contact.css"
+import { useEffect, useState } from 'react';
+import './contact.css';
 
-function Contact(){
-    return(
-        <div id={"contact"} className={"Tile Contact"}>
-            <h1 className={"AboutMeTitle"}>
-                <svg className={"contactSvg"} width="80px" height="80px" viewBox="0 0 24 24" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8 10H8.01" stroke="#2D8D9A" strokeWidth="2" strokeLinecap="round"
-                          strokeLinejoin="round"/>
-                    <path d="M12 10H12.01" stroke="#2D8D9A" strokeWidth="2" strokeLinecap="round"
-                          strokeLinejoin="round"/>
-                    <path d="M16 10H16.01" stroke="#2D8D9A" strokeWidth="2" strokeLinecap="round"
-                          strokeLinejoin="round"/>
-                    <path
-                        d="M21 13V7C21 5.11438 21 4.17157 20.4142 3.58579C19.8284 3 18.8856 3 17 3H7C5.11438 3 4.17157 3 3.58579 3.58579C3 4.17157 3 5.11438 3 7V13C3 14.8856 3 15.8284 3.58579 16.4142C4.17157 17 5.11438 17 7 17H7.5C7.77614 17 8 17.2239 8 17.5V20V20.1499C8 20.5037 8.40137 20.7081 8.6875 20.5L13.0956 17.2941C13.3584 17.103 13.675 17 14 17H17C18.8856 17 19.8284 17 20.4142 16.4142C21 15.8284 21 14.8856 21 13Z"
-                        stroke="#2D8D9A" strokeWidth="2" strokeLinejoin="round"/>
-                </svg>
-                <header className={"headerTextContact"}>Contact</header>
-            </h1>
-            <h2 className="ContactContent" title="email">
-                <svg width="50px"
-                     height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
-                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#FFFFFF"
-                       strokeWidth="0.048"/>
-                    <g id="SVGRepo_iconCarrier">
-                        <path
-                            d="M10 19H6.2C5.0799 19 4.51984 19 4.09202 18.782C3.71569 18.5903 3.40973 18.2843 3.21799 17.908C3 17.4802 3 16.9201 3 15.8V8.2C3 7.0799 3 6.51984 3.21799 6.09202C3.40973 5.71569 3.71569 5.40973 4.09202 5.21799C4.51984 5 5.0799 5 6.2 5H17.8C18.9201 5 19.4802 5 19.908 5.21799C20.2843 5.40973 20.5903 5.71569 20.782 6.09202C21 6.51984 21 7.0799 21 8.2V10M20.6067 8.26229L15.5499 11.6335C14.2669 12.4888 13.6254 12.9165 12.932 13.0827C12.3192 13.2295 11.6804 13.2295 11.0677 13.0827C10.3743 12.9165 9.73279 12.4888 8.44975 11.6335L3.14746 8.09863M14 21L16.025 20.595C16.2015 20.5597 16.2898 20.542 16.3721 20.5097C16.4452 20.4811 16.5147 20.4439 16.579 20.399C16.6516 20.3484 16.7152 20.2848 16.8426 20.1574L21 16C21.5523 15.4477 21.5523 14.5523 21 14C20.4477 13.4477 19.5523 13.4477 19 14L14.8426 18.1574C14.7152 18.2848 14.6516 18.3484 14.601 18.421C14.5561 18.4853 14.5189 18.5548 14.4903 18.6279C14.458 18.7102 14.4403 18.7985 14.405 18.975L14 21Z"
-                            stroke="#FFFFFF" strokeWidth="1.26" strokeLinecap="round" strokeLinejoin="round"/>
-                    </g>
-                </svg>
-                <p className={"contactText"}>krzysieksobol9@gmail.com</p>
-            </h2>
-            <h3 className="ContactContent" title="discord">
-                <svg width="50px" height="50px" viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" fill="none">
+function Contact() {
+    const [isVisible, setIsVisible] = useState(false);
 
-                    <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+    useEffect(() => {
+        // Set up intersection observer to trigger animation
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setIsVisible(entry.isIntersecting);
+            },
+            { threshold: 0.1 }
+        );
 
-                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+        const section = document.querySelector('.contact-section');
+        if (section) observer.observe(section);
 
-                    <g id="SVGRepo_iconCarrier">
+        return () => {
+            if (section) observer.unobserve(section);
+        };
+    }, []);
 
-                        <path stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-width="10"
-                              d="M120.755 170c.03-4.669.059-20.874.059-27.29 0-9.272-3.167-15.339-6.719-18.41 22.051-2.464 45.201-10.863 45.201-49.067 0-10.855-3.824-19.735-10.175-26.683 1.017-2.516 4.413-12.63-.987-26.32 0 0-8.296-2.672-27.202 10.204-7.912-2.213-16.371-3.308-24.784-3.352-8.414.044-16.872 1.14-24.785 3.352C52.457 19.558 44.162 22.23 44.162 22.23c-5.4 13.69-2.004 23.804-.987 26.32C36.824 55.498 33 64.378 33 75.233c0 38.204 23.149 46.603 45.2 49.067-3.551 3.071-6.719 9.138-6.719 18.41 0 6.416.03 22.621.059 27.29M27 130c9.939.703 15.67 9.735 15.67 9.735 8.834 15.199 23.178 10.803 28.815 8.265"/>
-
-                    </g>
-
-                </svg>
-                <p className={"contactText"}>github.com/KrzysztofSobol</p>
-            </h3>
-            <h4 className="ContactContent">
-                <svg width="49px" height="49px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-                     stroke="#000000" strokeWidth="1">
-
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
-
-                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"/>
-
-                    <g id="SVGRepo_iconCarrier">
-                        <path
-                            d="M22 3.47059V20.5294C22 20.9194 21.845 21.2935 21.5692 21.5693C21.2935 21.8451 20.9194 22 20.5294 22H3.47056C3.08053 22 2.70648 21.8451 2.43069 21.5693C2.15491 21.2935 1.99997 20.9194 1.99997 20.5294V3.47059C1.99997 3.08056 2.15491 2.70651 2.43069 2.43073C2.70648 2.15494 3.08053 2 3.47056 2H20.5294C20.9194 2 21.2935 2.15494 21.5692 2.43073C21.845 2.70651 22 3.08056 22 3.47059V3.47059ZM7.88232 9.64706H4.94115V19.0588H7.88232V9.64706ZM8.14703 6.41176C8.14858 6.18929 8.10629 5.96869 8.02258 5.76255C7.93888 5.55642 7.81539 5.36879 7.65916 5.21039C7.50294 5.05198 7.31705 4.92589 7.1121 4.83933C6.90715 4.75277 6.68715 4.70742 6.46468 4.70588H6.41173C5.95931 4.70588 5.52541 4.88561 5.20549 5.20552C4.88558 5.52544 4.70585 5.95934 4.70585 6.41176C4.70585 6.86419 4.88558 7.29809 5.20549 7.61801C5.52541 7.93792 5.95931 8.11765 6.41173 8.11765V8.11765C6.63423 8.12312 6.85562 8.0847 7.06325 8.00458C7.27089 7.92447 7.46071 7.80422 7.62186 7.65072C7.78301 7.49722 7.91234 7.31346 8.00245 7.10996C8.09256 6.90646 8.14169 6.6872 8.14703 6.46471V6.41176ZM19.0588 13.3412C19.0588 10.5118 17.2588 9.41177 15.4706 9.41177C14.8851 9.38245 14.3021 9.50715 13.7798 9.77345C13.2576 10.0397 12.8142 10.4383 12.4941 10.9294H12.4117V9.64706H9.64703V19.0588H12.5882V14.0529C12.5457 13.5403 12.7072 13.0315 13.0376 12.6372C13.368 12.2429 13.8407 11.9949 14.3529 11.9471H14.4647C15.4 11.9471 16.0941 12.5353 16.0941 14.0176V19.0588H19.0353L19.0588 13.3412Z"
-                            stroke="#FFFFFF" strokeLinejoin="round"/>
-                    </g>
-
-                </svg>
-                <p className={"contactText"}>linkedin.com/in/krzysztof-sobol</p>
-            </h4>
+    return (
+        <div id="contact" className="page contact-page">
+            <div className={`contact-section ${isVisible ? 'visible' : ''}`}>
+                <div className="content-container">
+                    <h2>Contact</h2>
+                    <div className="links-container">
+                        <a href="https://github.com/KrzysztofSobol"
+                           className="contact-link" target="_blank" rel="noopener noreferrer">
+                            <svg className="contact-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                            </svg>
+                            github.com/ksobolewski
+                        </a>
+                        <a href="mailto:krzysztofsobol9@gmail.com" className="contact-link">
+                            <svg className="contact-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-20.728c-.904 0-1.636-.732-1.636-1.636v-13.909c0-.904.732-1.636 1.636-1.636h20.728c.904 0 1.636.732 1.636 1.636zm-2.418 0l-8.946 7.908-8.946-7.908h17.892zm.782 13.909v-12.732l-9.728 8.574-9.728-8.574v12.732h19.456z"/>
+                            </svg>
+                            krzysztofsobol9@gmail.com
+                        </a>
+                        <a href="https://www.linkedin.com/in/krzysztof-sobol/"
+                           className="contact-link" target="_blank" rel="noopener noreferrer">
+                            <svg className="contact-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                            </svg>
+                            linkedin.com/in/krzysztof-sobol
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
