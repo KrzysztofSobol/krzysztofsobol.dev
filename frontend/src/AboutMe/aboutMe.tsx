b23b3621f41e421060cd7e72b11537c808bbec59
Kex <krzysieksobol9@gmail.com>
2025-03-02 20:09:31 +0100

    border radius changes

--- frontend/src/AboutMe/aboutMe.tsx
+++ frontend/src/AboutMe/aboutMe.tsx
@@ -59,7 +59,7 @@ function AboutMe() {
                                 <p>$ specialization: algorithms & data structures</p>
                             </div>
                         </div>
-                        <img src="/ProfilePicture.png" alt="Description of the image"/>
+                        <img className="profile-picture" src="/ProfilePicture.png" alt="Description of the image"/>
                     </div>
                 </div>
             </div>
