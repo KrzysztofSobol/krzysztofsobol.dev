255ac90249f3798148d46f7b51aba2e49b9c00f5
Kex <krzysieksobol9@gmail.com>
2025-03-09 20:29:58 +0100

    fixed nav highlight issue

--- frontend/src/Navbar/buttonHeader.tsx
+++ frontend/src/Navbar/buttonHeader.tsx
@@ -87,11 +87,21 @@ function ButtonHeader() {
                 <div className={"button-container"}>
                     <div onClick={() => handleScrollToSection('aboutMe')}>
                         <span className="highlight nav-button-text">about me</span>
-                        <button className="button nav-button-icon"> g </button>
+                        <button className="button nav-button-icon">
+                            <svg width="50px" height="50px" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <g id="User / User_01">
+                                    <path id="Vector"
+                                          d="M19 21C19 17.134 15.866 14 12 14C8.13401 14 5 17.134 5 21M12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7C16 9.20914 14.2091 11 12 11Z"
+                                          stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round"
+                                          strokeLinejoin="round"/>
+                                </g>
+                            </svg>
+                        </button>
                     </div>
                     <div onClick={() => handleScrollToSection('projects')}>
                         <span className="highlight nav-button-text">projects</span>
-                        <button className="button nav-button-icon"> g </button>
+                        <button className="button nav-button-icon"> g</button>
                     </div>
                     <div onClick={() => handleScrollToSection('contact')}>
                         <span className="highlight nav-button-text">contact</span>
