354aac5499be021ded837db91b95cdc6582ce6cd
Kex <krzysieksobol9@gmail.com>
2025-04-08 12:00:00 +0200

    map service doesnt override the axios error

--- frontend/src/services/mapService.ts
+++ frontend/src/services/mapService.ts
@@ -1,11 +1,7 @@
 import axiosInstance from "@/axiosConfig";
-import {mapData, mapParameters} from "@/types/mapType.ts";
+import { mapData, mapParameters } from "@/types/mapType.ts";
 
 export const getCustomMap = async (parameters: mapParameters): Promise<mapData> => {
-    try{
-        const response = await axiosInstance.get<mapData>(`modifiedMapData`,{params: parameters});
-        return response.data;
-    } catch (error) {
-        throw new Error('Failed to fetch map data');
-    }
+    const response = await axiosInstance.get<mapData>(`modifiedMapData`, { params: parameters });
+    return response.data;
 };
