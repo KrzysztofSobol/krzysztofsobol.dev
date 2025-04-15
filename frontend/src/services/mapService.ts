07e05e4ab6e9cc17061ac1f426b1c00b48e61ee7
Kex <krzysieksobol9@gmail.com>
2025-04-16 01:26:01 +0200

    decoding of the data from backend

--- frontend/src/services/mapService.ts
+++ frontend/src/services/mapService.ts
@@ -3,5 +3,40 @@ import { mapData, mapParameters } from "@/types/mapType.ts";
 
 export const getCustomMap = async (parameters: mapParameters): Promise<mapData> => {
     const response = await axiosInstance.get<mapData>(`modifiedMapData`, { params: parameters });
+
+    if (response.data) {
+        response.data = decodeMapData(response.data);
+    }
+
     return response.data;
+};
+
+export const decodeMapData = (encodedLines: string[]): string[] => {
+    return encodedLines.map(line => {
+        if (!line) return '';
+
+        let decoded = '';
+        let i = 0;
+
+        while (i < line.length) {
+            if (/\d/.test(line[i])) {
+                let count = '';
+                while (i < line.length && /\d/.test(line[i])) {
+                    count += line[i];
+                    i++;
+                }
+
+                if (i < line.length) {
+                    const char = line[i];
+                    decoded += char.repeat(parseInt(count));
+                    i++;
+                }
+            } else {
+                decoded += line[i];
+                i++;
+            }
+        }
+
+        return decoded;
+    });
 };
